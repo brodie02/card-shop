eb6911528d126ff24411a764091dd9210300349f
brodie02 <marshallbrodie0@gmail.com>
2023-03-05 20:55:21 +1030

    Add models

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const Order = require('./Order')
+
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: /.+\@.+\..+/,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+})
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
---
+++ server/models/index.js
@@ -0,0 +1,9 @@
+const User = require('./User')
+const Product = require('./Product')
+const Category = require('./Category')
+
+module.exports = {
+  User, 
+  Product,
+  Category
+}
