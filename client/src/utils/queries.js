92ce7fcbdfe683baa7916140f66ea85dba176801
brodie02 <marshallbrodie0@gmail.com>
2023-03-09 20:29:28 +1030

    Add query and mutations to client side

---
+++ client/src/utils/queries.js
@@ -0,0 +1,87 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PRODUCTS_BY_CATEGORY = gql`
+  query getProducts($categories: ID) {
+    products(categories: $categories) {
+      _id
+      name
+      description
+      price
+      stock
+      image
+      categories {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_PRODUCT = gql`
+  query getProduct($id: ID!) {
+    product(_id: $id) {
+      _id
+      name
+      description
+      price
+      stock
+      image
+      categories {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      stock
+      image
+      categories {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          stock
+          image
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
